654d820a62d2c6b60c0ab686b679427ae41bb78a
Mój Komp <fhadzlik9@gmail.com>
2021-02-24 18:40:16 +0100

    master

---
+++ Handlery/schematic.handler.js
@@ -0,0 +1,19 @@
+module.exports = (msg) => {
+    const { content } = msg
+  
+    const a = content.split("\n")
+  
+    const imie = a[0]
+    const wiek = a[1]
+    const zainteresowania = a[2]
+  
+    // Validation
+    if (
+      !imie.startsWith("Imię:") ||
+      !wiek.startsWith("Wiek:") ||
+      !zainteresowania.startsWith("Zainteresowania:")
+    )
+      msg.delete()
+  
+    console.log(a)
+  }
