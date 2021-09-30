2358e6f9c36a1bfe1ad76cf2b7abf1d74a161395
Mój Komp <fhadzlik9@gmail.com>
2021-09-30 13:21:52 +0200

    master

--- Config/config.js
+++ Config/config.js
@@ -4,7 +4,7 @@ const client = new Client({ intents: [] })
 const { Permissions: { FLAGS }, MessageEmbed } = require("discord.js")
 
 module.exports = {
-    token: process.env.token,
+    token: process.env.TOKEN,
     prefix: "/",
     owner: "622714126841675778",
     corona_api: "https://disease.sh/v3/covid-19",
