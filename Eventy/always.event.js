e547fbd81d66d26d6a12478208d91650bb135a1c
Mój Komp <fhadzlik9@gmail.com>
2022-04-03 18:09:12 +0200

    master

--- Eventy/always.event.js
+++ Eventy/always.event.js
@@ -8,10 +8,58 @@ module.exports = {
     async run(client) {
         console.log(chalk.green(`Zalogowano jako ${client.user.tag}!`));
         //wiadomosc pierwsza
-        client.user.setActivity("Programuje 24/7", {
-            type: "STREAMING",
-            url: "https://www.twitch.tv/samoyedfranek",
-            status: "dnd"
+        client.user.setPresence({
+            status: 'dnd'
+        })
+        setInterval(async () => {
+            setTimeout(function () {
+                client.user.setPresence({
+                    activities: [{
+                        name: `Aktualnie nic nie robię`,
+                        type: `PLAYING`
+                    }]
+                });
+            }, 15000);
+            setTimeout(function () {
+                client.user.setPresence({
+                    activities: [{
+                        name: `Serdecznie cię zapraszam na https://discord.gg/tPrEPhCB38`,
+                        type: `PLAYING`
+                    }]
+                });
+            }, 15000);
+            setTimeout(function () {
+                client.user.setPresence({
+                    activities: [{
+                        name: `Programuje 24/7`,
+                        type: `STREAMING`
+                    }]
+                });
+            }, 15000);
+            setTimeout(function () {
+                client.user.setPresence({
+                    activities: [{
+                        name: `Mata - Szafir`,
+                        type: `LISTENING`
+                    }]
+                });
+            }, 15000);
+            setTimeout(function () {
+                client.user.setPresence({
+                    activities: [{
+                        name: `Mata - Schodki`,
+                        type: `LISTENING`
+                    }]
+                });
+            }, 15000);
+            setTimeout(function () {
+                client.user.setPresence({
+                    activities: [{
+                        name: `Mata - Prawy do lewego`,
+                        type: `LISTENING`
+                    }]
+                });
+            }, 15000);
         });
         const embed = new MessageEmbed()
 
